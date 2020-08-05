810013bc83b9ef91544c8718535d1566e9005f4e
harkib <harkib@sfu.ca>
2020-08-05 12:11:47 -0700

    Part1b

--- main.m
+++ main.m
@@ -1,5 +1,7 @@
 %% MSE 481 - Project 2
-%% Part 1a
+%% Part 1
+
+% Part 1a: obtain continuous Laplace transform for the DC motor as a symbolic model
 
 J = 0.01; % kg.m^2
 b = 0.1; % N.m.s
@@ -10,3 +12,6 @@ L = 0.5; % H
 numerator = K;
 denominator = [J*L, J*R+b*L, b*R + K^2];
 sys = tf(numerator ,denominator );
+
+% Part 1b: Convert the obtained Laplace transform to the discrete z-domain
+sysd = c2d(sys,.05,'zoh');
